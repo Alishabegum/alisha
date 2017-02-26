311c079e78a5195e27dda270728d449f488087c9
Alishabegum <begumalisha98@gmail.com>
2017-02-26 15:21:13 +0530

    Add files via upload

---
+++ Final.java
@@ -0,0 +1,18 @@
+//FINAL VARIABLE USING CONSTRUCTOR
+class Final
+{  
+final int VALUE;
+//final keyword
+Password()
+{
+VALUE='alisha100';
+}
+void display()
+{  
+System.out.println(VALUE);
+}  
+public static void main(String args[]){  
+Final obj= new Final();  
+obj.display();  
+}  
+}
